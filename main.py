d82c05c87a8b2d75d56f0e6f3605358d1468b8fd
adaml <adaml>
2021-12-12 22:29:59 +0100

    push notebook

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ import nltk
 # nltk.download('all')
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+
 
 
 # Funcio per a llegir dades en format csv
@@ -56,61 +58,33 @@ if __name__ == '__main__':
     #plt.show()
 
     # label encoding for the taster name :
-
-    x = dataset.taster_name.values
-    y = dataset[['taster_name']].values
-
-
     lb = preprocessing.LabelEncoder()
-    dataset.loc[:, 'taster_name_encoded'] = lb.fit_transform(y)
+    dataset.loc[:, 'taster_name_encoded'] = lb.fit_transform(dataset.taster_name.values)
 
-    # Text Analysis begins :
-    comment_taster = text_data
-    comment_taster.isna().sum()  # handling missing values for taster
-    # testing whether we can get any data by overlapping and comparing the twitter handler and taster name
-    taster = []
-    taster_id = []
-    ind1 = comment_taster[comment_taster['taster_name'].isna() == True].index
-    ind2 = comment_taster[comment_taster['taster_twitter_handle'].isna() == True].index
-    common = set(ind1).intersection(set(ind2))
-    print(len(common))
-    comment_taster.drop(common, axis=0, inplace=True)
-    ind3 = comment_taster[comment_taster['taster_twitter_handle'].isna() == True].index
-    print(len(ind3))
-    comment_taster.drop('taster_twitter_handle', axis=1, inplace=True)
+    Y = dataset['taster_name_encoded']
+    X = dataset['description']
+    # Training and test split
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=42)
+    print("Shape of train predictors", X_train.shape)
+    print("Shape of test predictors", X_test.shape)
 
+    # Wordmatrix with TF-IDF
+    from sklearn import feature_extraction
 
-    x = comment_taster.taster_name.values
-    y = comment_taster[['taster_name']].values
-    lb = preprocessing.LabelEncoder()
-    comment_taster.loc[:, 'taster_name_encoded'] = lb.fit_transform(y)
+    td = feature_extraction.text.TfidfVectorizer()
+    word_train = td.fit_transform(X_train)
+    word_test = td.transform(X_test)
 
+    from sklearn.linear_model import LogisticRegression
+    text_model_lr = LogisticRegression(max_iter = 1000).fit(word_train, Y_train)
 
-    taster_and_code = comment_taster.loc[:, ['description', 'taster_name', 'taster_name_encoded']]
-    comment_taster.drop('taster_name', axis=1, inplace=True)
+    from sklearn.naive_bayes import MultinomialNB
+    text_model_nb = MultinomialNB().fit(word_train, Y_train)
 
+    y_predict_lr = text_model_lr.predict(word_test)
+    y_predict_nb = text_model_nb.predict(word_test)
 
-    # Text Normalization and preprocessing
-    j = 0
-    lemmatiser = WordNetLemmatizer()
-
-    for i in comment_taster.loc[:, 'description'].index:
-        final_out = ' '
-        sent = comment_taster.loc[i, 'description']
-        j = j + 1
-        # punctuation check
-        sentence1 = [char for char in sent if char not in string.punctuation]
-        sentence = ''.join(sentence1)
-        # lower case conversion
-        temp1 = sentence.lower()
-        # splitting
-        temp = temp1.split()
-        # stopwords removal
-        words = [word for word in temp if word not in stopwords.words('english')]
-        # lemmetizing the sentence
-        for word in words:
-            b = lemmatiser.lemmatize(word, pos="v")
-            final_out = final_out + b + ' '
-
-        comment_taster.loc[i, 'description'] = final_out
-
+    accuracy_lr = np.mean(y_predict_lr == Y_test)
+    accuracy_nb = np.mean(y_predict_nb == Y_test)
+    print("Accuracy of Logistic Model ==", accuracy_lr)
+    print("Accuracy of Naives Model ==", accuracy_nb)
